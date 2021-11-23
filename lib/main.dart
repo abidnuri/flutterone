16d800ad3c09ba7e24891d8bbe15659ba31f5d7e
Abid Nuri <mdabidnuri@gmail.com>
2021-11-23 11:32:15 +0600

    bd flag art

--- lib/main.dart
+++ lib/main.dart
@@ -12,59 +12,91 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('''Abid App'''),
-        ),
-        drawer: const Drawer(),
-        // body: Container(
-        //     height: 300,
-        //     width: 300,
-        //     color: Colors.cyan,
-        //     alignment: Alignment.center,
-        //     child: Container(
-        //       height: 150,
-        //       width: 150,
-        //       color: Colors.black,
-        //       alignment: Alignment.center,
-        //       child: Container(
-        //         height: 50,
-        //         width: 50,
-        //         color: Colors.red,
-        //         alignment: Alignment.center,
-        //       ),
-        //     )
-        //     )
-        body: Container(
-            height: 600,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  height: 50,
-                  width: 50,
-                  // margin: const EdgeInsets.all(10),
-                  color: Colors.cyan,
-                ),
-                Container(
-                  height: 50,
-                  width: 50,
-                  // margin: const EdgeInsets.all(10),
-                  color: Colors.red,
-                ),
-                Container(
-                  height: 50,
-                  width: 50,
-                  // margin: const EdgeInsets.all(10),
-                  color: Colors.green,
-                ),
-                Container(
-                  height: 50,
-                  width: 50,
-                  //margin: const EdgeInsets.all(10),
-                  color: Colors.blue,
-                ),
-              ],
-            )));
+      appBar: AppBar(
+        title: const Text('''Abid App'''),
+      ),
+      drawer: const Drawer(),
+      // body: Container(
+      //     height: 300,
+      //     width: 300,
+      //     color: Colors.cyan,
+      //     alignment: Alignment.center,
+      //     child: Container(
+      //       height: 150,
+      //       width: 150,
+      //       color: Colors.black,
+      //       alignment: Alignment.center,
+      //       child: Container(
+      //         height: 50,
+      //         width: 50,
+      //         color: Colors.red,
+      //         alignment: Alignment.center,
+      //       ),
+      //     )
+      //     )
+
+      // 7th Class
+
+      // body: Container(
+      //     height: 600,
+      //     child: Row(
+      //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //       crossAxisAlignment: CrossAxisAlignment.center,
+      //       children: [
+      //         Container(
+      //           height: 50,
+      //           width: 50,
+      //           // margin: const EdgeInsets.all(10),
+      //           color: Colors.cyan,
+      //         ),
+      //         Container(
+      //           height: 50,
+      //           width: 50,
+      //           // margin: const EdgeInsets.all(10),
+      //           color: Colors.red,
+      //         ),
+      //         Container(
+      //           height: 50,
+      //           width: 50,
+      //           // margin: const EdgeInsets.all(10),
+      //           color: Colors.green,
+      //         ),
+      //         Container(
+      //           height: 50,
+      //           width: 50,
+      //           //margin: const EdgeInsets.all(10),
+      //           color: Colors.blue,
+      //         ),
+      //       ],
+      //     ))
+      //
+
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              height: 350,
+              width: 10,
+              color: Colors.black,
+              margin: const EdgeInsets.all(2)),
+          Container(
+            height: 50,
+            width: 150,
+            color: Colors.green,
+            margin: const EdgeInsets.all(5),
+            alignment: Alignment.center,
+            child: Container(
+              height: 30,
+              width: 30,
+              // color: Colors.red,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
