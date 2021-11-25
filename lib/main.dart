b0fbed161d1b1943fb2adc510cf8ac934b2cbc7a
Abid Nuri <mdabidnuri@gmail.com>
2021-11-25 10:50:02 +0600

    row expanded/ responsiveness

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,38 @@ class MyApp extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('''Abid App'''),
+        centerTitle: true,
       ),
       drawer: const Drawer(),
-      body: Row(),
+      body: Row(
+        children: [
+          //Use "ctrl+." for wrap something
+          Expanded(
+            flex: 1,
+            child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.red,
+            ),
+          ),
+          Expanded(
+            flex: 4,
+            child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.black,
+            ),
+          ),
+          Expanded(
+            flex: 5, // 60% of space => (6/(6 + 4))
+            child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.green,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
