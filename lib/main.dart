efd29299fd5a2b7fb9000595704d028f7b4312a3
Abid Nuri <mdabidnuri@gmail.com>
2021-11-25 12:01:11 +0600

    column then row, alignment,row

--- lib/main.dart
+++ lib/main.dart
@@ -17,44 +17,80 @@ class MyApp extends StatelessWidget {
         centerTitle: true,
       ),
       drawer: const Drawer(),
-      body: Row(
+      body: Column(
         children: [
-          //Use "ctrl+." for wrap something
-          Expanded(
-            flex: 1,
-            child: Container(
-              height: 100,
-              width: 100,
-              color: Colors.red,
-            ),
-          ),
-          Expanded(
-            flex: 4,
-            child: Container(
-              height: 100,
-              width: 100,
-              color: Colors.black,
-            ),
-          ),
-          Expanded(
-            flex: 5, // 60% of space => (6/(6 + 4))
-            child: Container(
-              height: 100,
-              width: 100,
-              color: Colors.green,
-              alignment: Alignment.center,
-              child: Container(
-                height: 50,
-                width: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.cyan,
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 400,
+                  width: 400,
+                  color: Colors.brown,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 50,
+                        color: Colors.amber,
+                      ),
+                      Container(
+                        height: 50,
+                        width: 50,
+                        color: Colors.red,
+                      ),
+                      Container(
+                        height: 50,
+                        width: 50,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
         ],
-      ),
+      ), // part two
+
+      // Row(
+      //   children: [
+      //     //Use "ctrl+." for wrap something
+      //     Expanded(
+      //       flex: 1,
+      //       child: Container(
+      //         height: 100,
+      //         width: 100,
+      //         color: Colors.red,
+      //       ),
+      //     ),
+      //     Expanded(
+      //       flex: 4,
+      //       child: Container(
+      //         height: 100,
+      //         width: 100,
+      //         color: Colors.black,
+      //       ),
+      //     ),
+      //     Expanded(
+      //       flex: 5, // 60% of space => (6/(6 + 4))
+      //       child: Container(
+      //         height: 100,
+      //         width: 100,
+      //         color: Colors.green,
+      //         alignment: Alignment.center,
+      //         child: Container(
+      //           height: 50,
+      //           width: 50,
+      //           decoration: BoxDecoration(
+      //             borderRadius: BorderRadius.circular(10),
+      //             color: Colors.cyan,
+      //           ),
+      //         ),
+      //       ),
+      //     ),
+      //   ],
+      // ),//Part One
     );
   }
 }
