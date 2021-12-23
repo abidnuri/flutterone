7fd116dce1133022b23ef490461d79baf8e9fbf0
Abid Nuri <mdabidnuri@gmail.com>
2021-12-23 12:44:45 +0600

    stack with design of a shopping page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'package:abid/Bag.dart';
-import 'package:abid/Discount.dart';
-import 'package:abid/cart.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,111 +7,152 @@ void main() {
   ));
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  var pages = [
-    const Bag(),
-    const Cart(),
-    const Discount(),
-  ];
-  var currentPage = 0;
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(),
       appBar: AppBar(
-        title: const Text('''Abid App'''),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        shadowColor: Colors.green,
-        elevation: 80,
-        leading: Image.network(
-          'https://cdn.pixabay.com/photo/2017/05/26/16/08/glass-2346358_1280.png',
-          fit: BoxFit.fill,
-        ),
+        title: Text('Abid Shop'),
+        backgroundColor: Colors.pink,
         actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.shopping_bag),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.shopping_basket),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.shopping_cart),
-          ),
+          // IconButton(
+          //   onPressed: () {},
+          //   icon: CircleAvatar(
+          //     radius: 50,
+          //     backgroundColor: Colors.green,
+          //     child: Icon(Icons.shopping_cart),
+          //   ),
+          // ),
+          CircleAvatar(
+              radius: 20,
+              backgroundColor: Colors.white,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.shopping_cart),
+              )),
         ],
-        actionsIconTheme: const IconThemeData(
-          color: Colors.red,
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentPage,
-        onTap: (value) {
-          setState(() {
-            currentPage = value;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_bag),
-            label: 'Bag',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: 'Cart',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.event_available),
-            label: 'Discount',
-          ),
-        ],
-        backgroundColor: Colors.green,
-        selectedItemColor: Colors.amber,
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-              ),
-            ),
-            ListTile(
-              title: const Text('Home'),
-              leading: const Icon(Icons.home),
-              onLongPress: () {},
-            ),
-            ListTile(
-              title: const Text('About'),
-              leading: const Icon(Icons.account_box_rounded),
-              onLongPress: () {},
-            ),
-          ],
-        ),
+        elevation: 10,
       ),
       body: Column(
         children: [
-          Center(
-            child: AspectRatio(
-              aspectRatio: 15,
-              child: Container(
-                color: Colors.amber,
-                child: const Center(
-                  child: Text("Drinks Well"),
-                ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              width: double.infinity,
+              color: Colors.white,
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    // color: Colors.grey,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(100),
+                      ),
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.pink,
+                          Colors.purple,
+                        ],
+                        begin: Alignment.topCenter, //direction of color
+                        end: Alignment.bottomCenter, //direction of color
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "আমার জুতা",
+                            style: TextStyle(
+                              fontSize: 24,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                color: Colors.white,
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                '4.5',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "Size - 9",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "Brand : Adidas",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: -10,
+                    left: 28,
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          child: Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          ),
+                          backgroundColor: Colors.white,
+                        ),
+                        Image.network(
+                            "https://freepngimg.com/thumb/shoes/27428-5-nike-shoes-transparent-background.png")
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
-          pages[currentPage],
+          SizedBox(
+            height: MediaQuery.of(context).size.height / 12,
+          ),
+          Expanded(
+            flex: 4,
+            child: Container(
+              width: double.infinity,
+              color: Colors.red,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              width: double.infinity,
+              color: Colors.blue,
+            ),
+          ),
         ],
       ),
     );
